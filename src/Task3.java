d0d407bd03dd9155788a3f1e3f71275b8c9fabe8
AllaKulchitskaya <alla070@list.ru>
2022-11-14 21:17:40 +0300

    Задача 3

---
+++ src/Task3.java
@@ -0,0 +1,22 @@
+public class Task3 {
+    public static void main(String[] args) {
+        // Пункт 1
+        int[] array1 = new int [3];
+        array1[0] = 1;
+        array1[1] = 2;
+        array1[2] = 3;
+        System.out.print(array1[2] + ", ");
+        System.out.print(array1[1] + ", ");
+        System.out.println(array1[0] + " ");
+        // Пункт 2
+        double[] array2 = {1.57, 7.654, 9.986};
+        System.out.print(array2[2] + ", ");
+        System.out.print(array2[1] + ", ");
+        System.out.println(array2[0] + " ");
+        // Пункт 3
+        byte[] array3 = {25, 65, 38};
+        System.out.print(array3[2] + ", ");
+        System.out.print(array3[1] + ", ");
+        System.out.println(array3[0] + " ");
+    }
+}
