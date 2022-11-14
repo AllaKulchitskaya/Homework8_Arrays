4352b4f466a15cb0b5c6796bcc921fc0d55b098a
AllaKulchitskaya <alla070@list.ru>
2022-11-14 21:08:45 +0300

    Задача 1

--- src/Task1.java
+++ src/Task1.java
@@ -1,4 +1,22 @@
 public class Task1 {
     public static void main(String[] args) {
+        // Пункт 1
+        int[] array1 = new int [3];
+        array1[0] = 1;
+        array1[1] = 2;
+        array1[2] = 3;
+        System.out.print(array1[0] + " ");
+        System.out.print(array1[1] + " ");
+        System.out.println(array1[2] + " ");
+        // Пункт 2
+        double[] array2 = {1.57, 7.654, 9.986};
+        System.out.print(array2[0] + " ");
+        System.out.print(array2[1] + " ");
+        System.out.println(array2[2] + " ");
+        // Пункт 3
+        byte[] array3 = {25, 65, 38};
+        for (byte i = 0; i < array3.length; i++) {
+            System.out.print(array3[i] + " ");
+        }
     }
 }
